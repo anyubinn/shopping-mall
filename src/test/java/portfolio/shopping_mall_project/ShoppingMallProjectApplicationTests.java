e27eb76b24a263b3ce20c5358a12286c3d4b4b1f
anyubinn <anyubin0724@naver.com>
2026-07-11 16:41:15 +0900

    feat: 초기 환경 세팅

---
+++ src/test/java/portfolio/shopping_mall_project/ShoppingMallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.shopping_mall_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
